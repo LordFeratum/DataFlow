fc1cb60650d890918ad146dcb227c2bbfa2161d0
Quique Piña <epina@apsl.net>
2018-06-22 12:42:48 +0200

    Added support to takewhile, dropwhile and sort in Enum module

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="DataFlow",
-    version="0.2",
+    version="0.3",
     description="A system transform data consistently and easily",
     packages=["dataflow"],
 )
