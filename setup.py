3ef8340d6c39b280ac06b3d0cf7268ebfedb0671
Quique Piña <epina@apsl.net>
2018-07-19 13:34:15 +0200

    Added metdatada to setup.py

--- setup.py
+++ setup.py
@@ -5,4 +5,8 @@ setup(
     version="0.3",
     description="A system transform data consistently and easily",
     packages=["dataflow"],
+    url='https://github.com/LordFeratum/DataFlow',
+    download_url='https://github.com/LordFeratum/DataFlow/archive/0.3.tar.gz',
+    author="Enrique Pina Monserrat",
+    author_email="lord.feratum@gmail.com"
 )
